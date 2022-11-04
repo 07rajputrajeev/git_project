f429bbd33a96fc38349af95e07e816ec2b38f429
swetanameste <sweta235410@gmail.com>
2022-11-04 19:03:46 +0530

    i have fixed the code

--- prime.js
+++ prime.js
@@ -1,8 +1,15 @@
-function checkPrime(num)
-for(let i=1; i<num; i--)
+function checkPrime(num){
+let count=0;
+for(let i=1; i<=num; i++)
 {
-if(num%i){
-return true;
+if(num%i==0){
+count++
 }
 }
-return false;
+if(count==2){
+console.log(num,"prime")
+}else{
+console.log("Not a prime")
+}
+}
+checkPrime(17);
